12d26298b6b1edcc59309506d40adee39790a35e
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2026-01-10 20:50:23 -0300

    Ajuste nas variaveis de ambiente

--- jumperfour/settings.py
+++ jumperfour/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5*fx=)er&%b%7jwvzxsoizfs+4_z%&)*3!)b5=m=f_bmy1hh)3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5*fx=)er&%b%7jwvzxsoizfs+4_z%&)*3!)b5=m=f_bmy1hh)3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
