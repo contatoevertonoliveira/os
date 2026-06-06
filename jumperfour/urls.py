6f1763185ef4651818f0ce52f3114b69d4e28e2d
Everton Oliveira <everton.oliveira@jumperfour.com>
2026-06-06 19:07:14 -0300

    Ajuste no favicon

--- jumperfour/urls.py
+++ jumperfour/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static as static_url
 
 handler403 = 'jumperfour.views.permission_denied'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=static_url('favicon/favicon.ico'), permanent=True)),
     path('', include('tickets.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
